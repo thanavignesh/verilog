afe2dc707153451312962b963a8fd6eacdd9a7d1
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2022-09-10 23:23:13 +0530

    Delete 8x1mux/work directory

--- 8x1mux/work/mux8to1/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mux8to1 is
-    port(
-        i               : in     vl_logic_vector(7 downto 0);
-        s               : in     vl_logic_vector(2 downto 0);
-        o               : out    vl_logic
-    );
-end mux8to1;
