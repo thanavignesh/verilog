47f04f0fc18cd8f319b8d3bc074add45f323cd24
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2022-09-10 23:18:20 +0530

    Delete 8X3 encoder/work directory

--- 8X3 encoder/work/enco8to3/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity enco8to3 is
-    port(
-        a               : in     vl_logic_vector(7 downto 0);
-        b               : out    vl_logic_vector(2 downto 0)
-    );
-end enco8to3;
