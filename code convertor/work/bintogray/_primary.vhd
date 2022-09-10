b7ecf6807dbf9fdac431895a50956434c61ba685
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2022-09-10 23:38:30 +0530

    Delete code convertor/work directory

--- code convertor/work/bintogray/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity bintogray is
-    port(
-        b               : in     vl_logic_vector(3 downto 0);
-        g               : out    vl_logic_vector(3 downto 0)
-    );
-end bintogray;
--- code convertor/work/graytobin/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity graytobin is
-    port(
-        G               : in     vl_logic_vector(3 downto 0);
-        b               : out    vl_logic_vector(3 downto 0)
-    );
-end graytobin;
